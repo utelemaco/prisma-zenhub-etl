17fb699a2e8670dfdf12820b969b919e5c67c1bd
Ulisses Telemaco <ulisses.telemaco@gmail.com>
2018-10-17 15:52:41 -0300

    Adjusting NullpointerException loading issues from github

--- src/main/java/org/prisma/zenhubetl/GithubAPI.groovy
+++ src/main/java/org/prisma/zenhubetl/GithubAPI.groovy
@@ -18,28 +18,28 @@ class GithubAPI extends AbstractAPI {
 	
 	private String appendAccessToken() {
 		if (accessToken) {
-			return "?access_token=${accessToken}"
+			return "access_token=${accessToken}"
 		}
 		return ''
 	}
 
 	public GithubEndpoints getEndpoints() {
-		def url = "${githubapi}${appendAccessToken()}"
+		def url = "${githubapi}?${appendAccessToken()}"
 		callExternalAPI(url, GithubEndpoints.class)
 	}
 	
 	public List<GithubMilestone> getMilestones(String owner, String repository) {
-		def url = "${githubapi}/repos/${owner}/${repository}/milestones${appendAccessToken()}"
+		def url = "${githubapi}/repos/${owner}/${repository}/milestones?${appendAccessToken()}"
 		callExternalAPI(url, GithubMilestone[].class)
 	}
 	
 	public List<GithubIssue> getIssues(String owner, String repository) {
-		def url = "${githubapi}/repos/${owner}/${repository}/issues${appendAccessToken()}"
+		def url = "${githubapi}/repos/${owner}/${repository}/issues?state=all&${appendAccessToken()}"
 		callExternalAPI(url, GithubIssue[].class)
 	}
 	
 	public List<GithubComment> getComments(String owner, String repository, def issueId) {
-		def url = "${githubapi}/repos/${owner}/${repository}/issues/${issueId}/comments${appendAccessToken()}"
+		def url = "${githubapi}/repos/${owner}/${repository}/issues/${issueId}/comments?${appendAccessToken()}"
 		callExternalAPI(url, GithubComment[].class)
 	}
 	
