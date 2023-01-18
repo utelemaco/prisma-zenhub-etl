cea5c1ef1aac288f42081c8ea1a24f1b7e147616
Ulisses Telemaco <ulisses.telemaco@gmail.com>
2023-01-18 01:34:03 -0500

    Changing Zenhub API authentication, from query string to http header.

--- src/main/java/org/prisma/zenhubetl/AbstractAPI.groovy
+++ src/main/java/org/prisma/zenhubetl/AbstractAPI.groovy
@@ -1,11 +1,10 @@
 package org.prisma.zenhubetl
 
-import org.prisma.zenhubetl.dto.GithubEndpoints
+
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpMethod
 import org.springframework.http.HttpStatus
-import org.springframework.http.MediaType
 import org.springframework.web.client.RestTemplate
 
 class AbstractAPI {
@@ -19,15 +18,18 @@ class AbstractAPI {
 		try {
 			RestTemplate restTemplate = new RestTemplate()
 			HttpHeaders headers = new HttpHeaders();
-			headers.add("Accept", "application/vnd.github.mockingbird-preview");
-			headers.add("Authorization", accessToken);
+			headers.add('Accept', 'application/vnd.github.mockingbird-preview');
+			if (accessToken) {
+				headers.add('Authorization', accessToken);
+				headers.add('X-Authentication-Token', accessToken);
+			}
 
-			HttpEntity<String> httpEntity = new HttpEntity<>("body", headers);
+			HttpEntity<String> httpEntity = new HttpEntity<>('body', headers);
 			//def response = restTemplate.getForEntity(url, responseClass)
 			def response = restTemplate.exchange(url, HttpMethod.GET, httpEntity, responseClass)
 
 			if (response.statusCode != HttpStatus.OK) {
-				throw new Exception("Error accessing external api.")
+				throw new Exception('Error accessing external api.')
 			}
 			return response.body
 		} catch(Exception e) {
@@ -35,17 +37,17 @@ class AbstractAPI {
 				throw e;
 			}
 			println "[${numberOfAttempts}] error $e"
-			println "  Trying again in 60 seconds..."
+			println '  Trying again in 60 seconds...'
 			sleep(10000)
-			println "  Trying again in 50 seconds..."
+			println '  Trying again in 50 seconds...'
 			sleep(10000)
-			println "  Trying again in 40 seconds..."
+			println '  Trying again in 40 seconds...'
 			sleep(10000)
-			println "  Trying again in 30 seconds..."
+			println '  Trying again in 30 seconds...'
 			sleep(10000)
-			println "  Trying again in 20 seconds..."
+			println '  Trying again in 20 seconds...'
 			sleep(10000)
-			println "  Trying again in 10 seconds..."
+			println '  Trying again in 10 seconds...'
 			sleep(10000)
 			return callExternalAPI(url, responseClass, ++numberOfAttempts)
 		}
--- src/main/java/org/prisma/zenhubetl/ZenhubAPI.groovy
+++ src/main/java/org/prisma/zenhubetl/ZenhubAPI.groovy
@@ -14,37 +14,30 @@ class ZenhubAPI extends AbstractAPI {
 		this.accessToken = accessToken;
 	}
 
-	private appendAccessToken() {
-		if (accessToken) {
-			return "?access_token=${accessToken}"
-		}
-		''
-	}
-
-	private static final String zenhubapi = 'https://api.zenhub.io'
+	private static final String zenhubapi = 'https://api.zenhub.com'
 
 	public ZenhubIssue getZenhubIssue(String repoId, def issueNumber) {
-		def url = "${zenhubapi}/p1/repositories/${repoId}/issues/${issueNumber}${appendAccessToken()}"
-		callExternalAPI(url, ZenhubIssue.class)
+		def url = "${zenhubapi}/p1/repositories/${repoId}/issues/${issueNumber}"
+		callExternalAPI(url, ZenhubIssue.class, accessToken)
 	}
 	
 	public ZenhubBoard getZenhubBoard(String repoId) {
-		def url = "${zenhubapi}/p1/repositories/${repoId}/board${appendAccessToken()}"
-		callExternalAPI(url, ZenhubBoard.class)
+		def url = "${zenhubapi}/p1/repositories/${repoId}/board"
+		callExternalAPI(url, ZenhubBoard.class, accessToken)
 	}
 	
 	public ZenhubEpic getZenhubEpic(String repoId, def issueNumber) {
-		def url = "${zenhubapi}/p1/repositories/${repoId}/epics/${issueNumber}${appendAccessToken()}"
-		callExternalAPI(url, ZenhubEpic.class)
+		def url = "${zenhubapi}/p1/repositories/${repoId}/epics/${issueNumber}"
+		callExternalAPI(url, ZenhubEpic.class, accessToken)
 	}
 	
 	public ZenhubDependencies getZenhubDependencies(String repoId) {
-		def url = "${zenhubapi}/p1/repositories/${repoId}/dependencies${appendAccessToken()}"
-		callExternalAPI(url, ZenhubDependencies.class)
+		def url = "${zenhubapi}/p1/repositories/${repoId}/dependencies"
+		callExternalAPI(url, ZenhubDependencies.class, accessToken)
 	}
 
 	public ZenhubMilestoneStartDate getZenhubMilestoneStartDate(String repoId, String milestoneNumber) {
-		def url = "${zenhubapi}/p1/repositories/${repoId}/milestones/${milestoneNumber}/start_date${appendAccessToken()}"
-		callExternalAPI(url, ZenhubMilestoneStartDate.class)
+		def url = "${zenhubapi}/p1/repositories/${repoId}/milestones/${milestoneNumber}/start_date"
+		callExternalAPI(url, ZenhubMilestoneStartDate.class, accessToken)
 	}
 }
